ad9be880b451282ae568ffc9bc65f5915dcd1bc3
Arranque 4 <Arranque 4@192.168.5.106>
2018-12-07 09:25:54 +0100

    Modificado editar

--- src/main/java/com/sopra/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/sopra/springboot/app/models/dao/ClienteDaoImpl.java
@@ -32,9 +32,15 @@ public class ClienteDaoImpl implements IClienteDao
 	@Transactional
 	public void save(Cliente cliente) 
 	{
-		em.persist(cliente);
+		if(cliente.getId() != null && cliente.getId() > 0)
+			em.merge(cliente);
+		else
+			em.persist(cliente);
 	}
 	
-	
-
+	@Override
+	public Cliente findOne(Long id) 
+	{
+		return em.find(Cliente.class, id);
+	}
 }
